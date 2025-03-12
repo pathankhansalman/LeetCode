3a38bbf7383618a221c7aeaab4cf05db08e7b0ed
Pathan Salman Khan <pathankhan.salman@gmail.com>
2025-03-12 14:00:00 +0000

    [March] Edit distance skeleton

--- edit-distance.py
+++ edit-distance.py
@@ -1,29 +1,3 @@
 class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        """
-        Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
-        
-        You have the following three operations permitted on a word:
-        - Insert a character
-        - Delete a character
-        - Replace a character
-        """
-        m, n = len(word1), len(word2)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        
-        for i in range(1, m + 1):
-            dp[i][0] = i
-        for j in range(1, n + 1):
-            dp[0][j] = j
-            
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if word1[i - 1] == word2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1]
-                else:
-                    dp[i][j] = min(dp[i - 1][j],    # Deletion
-                                   dp[i][j - 1],    # Insertion
-                                   dp[i - 1][j - 1] # Replacement
-                                  ) + 1
-                                  
-        return dp[m][n]
+    def minDistance(self, word1, word2):
+        pass
