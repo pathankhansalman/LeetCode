05ac0560add26bd2b7937a897a548ba1ec77daee
Pathan Salman Khan <pathankhan.salman@gmail.com>
2025-05-24 14:00:00 +0100

    [May] Longest consecutive sequence hashset solution

--- longest-consecutive-sequence.py
+++ longest-consecutive-sequence.py
@@ -1,3 +1,13 @@
 class Solution:
-    def longestConsecutive(self, nums):
-        pass
+    def longestConsecutive(self, nums: list[int]) -> int:
+        num_set = set(nums)
+        longest = 0
+        for num in num_set:
+            if num - 1 not in num_set:
+                curr = num
+                streak = 1
+                while curr + 1 in num_set:
+                    curr += 1
+                    streak += 1
+                longest = max(longest, streak)
+        return longest
