71f0013ad62f046d11a9ea2c257db4f1678113ff
Pathan Salman Khan <pathankhan.salman@gmail.com>
2025-07-09 12:00:00 +0100

    Add DP solution for LeetCode 322: Coin Change

---
+++ coin-change.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        """
+        You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+        Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+        You may assume that you have an infinite number of each kind of coin.
+        """
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+                
+        return int(dp[amount]) if dp[amount] != float('inf') else -1
